4ff2f696125f1bf40e5cca27fee342713c17cc71
tahaglbz <tahaglbz1@gmail.com>
2024-08-29 14:13:03 +0300

    fullscreen added

--- lib/screens/education/education.dart
+++ lib/screens/education/education.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_app/extensions/media_query.dart';
+import 'package:my_app/screens/education/fullscreen.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:get/get.dart';
-import 'package:my_app/extensions/media_query.dart';
 
 class Education extends StatefulWidget {
   const Education({super.key});
@@ -20,7 +21,6 @@ class _EducationState extends State<Education> {
   Widget build(BuildContext context) {
     final double deviceWidth = context.deviceWidth;
     double appBarHeight = deviceWidth * 0.28;
-
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(appBarHeight),
@@ -30,8 +30,6 @@ class _EducationState extends State<Education> {
             onPressed: () => Get.offAllNamed('/mainmenu'),
             icon: const Icon(Icons.arrow_back_ios_new_sharp),
           ),
-          automaticallyImplyLeading: false,
-          elevation: 0,
           title: Text(
             'Education',
             style: GoogleFonts.adamina(
@@ -48,21 +46,19 @@ class _EducationState extends State<Education> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Başlık Bölümü
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
                 'Videos',
                 style: GoogleFonts.adamina(
-                  fontSize: 24,
+                  fontSize: 16,
                   fontWeight: FontWeight.w700,
                   color: Colors.black,
                 ),
               ),
             ),
-            // Yatay Video Listesi
             Container(
-              height: 300, // Gerekirse yüksekliği ayarlayın
+              height: 300,
               child: StreamBuilder<QuerySnapshot>(
                 stream: eduCollection.snapshots(),
                 builder: (context, snapshot) {
@@ -82,60 +78,63 @@ class _EducationState extends State<Education> {
                       var videoTitle = videoData['title'];
                       var videoDescription = videoData['description'];
 
-                      YoutubePlayerController _controller =
-                          YoutubePlayerController(
-                        initialVideoId: YoutubePlayer.convertUrlToId(videoUrl)!,
-                        flags: const YoutubePlayerFlags(
-                          autoPlay: false,
-                          mute: false,
-                        ),
-                      );
-
-                      return Container(
-                        width: deviceWidth * 0.7,
-                        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                        child: Card(
-                          color: Colors.black87,
-                          elevation: 5,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                AspectRatio(
-                                  aspectRatio:
-                                      16 / 9, // YouTube videoları için orantı
-                                  child: YoutubePlayer(
-                                    controller: _controller,
-                                    showVideoProgressIndicator: true,
-                                    progressColors: const ProgressBarColors(
-                                      playedColor: Colors.red,
-                                      handleColor: Colors.redAccent,
+                      return GestureDetector(
+                        onTap: () {
+                          Get.to(() => FullScreenVideo(videoUrl: videoUrl));
+                        },
+                        child: Container(
+                          width: deviceWidth * 0.7,
+                          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Card(
+                            color: Colors.black87,
+                            elevation: 5,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  AspectRatio(
+                                    aspectRatio: 16 / 9,
+                                    child: YoutubePlayer(
+                                      controller: YoutubePlayerController(
+                                        initialVideoId:
+                                            YoutubePlayer.convertUrlToId(
+                                                videoUrl)!,
+                                        flags: const YoutubePlayerFlags(
+                                          autoPlay: false,
+                                          mute: false,
+                                        ),
+                                      ),
+                                      showVideoProgressIndicator: true,
+                                      progressColors: const ProgressBarColors(
+                                        playedColor: Colors.red,
+                                        handleColor: Colors.redAccent,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 8.0),
-                                  child: Text(
-                                    videoTitle,
-                                    style: GoogleFonts.adamina(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.white,
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 8.0),
+                                    child: Text(
+                                      videoTitle,
+                                      style: GoogleFonts.adamina(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.white,
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(top: 4.0),
-                                  child: Text(
-                                    videoDescription,
-                                    style: GoogleFonts.adamina(
-                                      fontSize: 14,
-                                      color: Colors.blueAccent,
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 4.0),
+                                    child: Text(
+                                      videoDescription,
+                                      style: GoogleFonts.adamina(
+                                        fontSize: 14,
+                                        color: Colors.blueAccent,
+                                      ),
                                     ),
                                   ),
-                                ),
-                              ],
+                                ],
+                              ),
                             ),
                           ),
                         ),
@@ -143,31 +142,6 @@ class _EducationState extends State<Education> {
                     },
                   );
                 },
-              ),
-            ),
-            // Makaleler Bölümü
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Articles',
-                style: GoogleFonts.adamina(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Container(
-              color: Colors.grey[900],
-              height: 400, // Gerekirse yüksekliği ayarlayın
-              child: Center(
-                child: Text(
-                  'Makaleler içeriği burada',
-                  style: GoogleFonts.adamina(
-                    fontSize: 18,
-                    color: Colors.white,
-                  ),
-                ),
               ),
             ),
           ],
--- lib/screens/education/fullscreen.dart
+++ lib/screens/education/fullscreen.dart
@@ -1,7 +1,9 @@
-// ignore_for_file: no_leading_underscores_for_local_identifiers
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_app/extensions/media_query.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+import '../../widgets/appColors.dart';
 
 class FullScreenVideo extends StatelessWidget {
   final String videoUrl;
@@ -10,6 +12,8 @@ class FullScreenVideo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = context.deviceWidth;
+    double appBarHeight = deviceWidth * 0.28;
     YoutubePlayerController _controller = YoutubePlayerController(
       initialVideoId: YoutubePlayer.convertUrlToId(videoUrl)!,
       flags: const YoutubePlayerFlags(
@@ -19,19 +23,31 @@ class FullScreenVideo extends StatelessWidget {
     );
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Video'),
-        backgroundColor: const Color.fromARGB(255, 0, 9, 99),
-      ),
-      body: Center(
-        child: YoutubePlayer(
-          controller: _controller,
-          showVideoProgressIndicator: true,
-          progressColors: const ProgressBarColors(
-            playedColor: Colors.red,
-            handleColor: Colors.redAccent,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(appBarHeight),
+        child: AppBar(
+          title: const Text('Video'),
+          backgroundColor: AppColors.defaultColor,
+          leading: IconButton(
+            onPressed: () => Get.offAllNamed('/mainmenu'),
+            icon: const Icon(Icons.arrow_back_ios_new_sharp),
           ),
+          automaticallyImplyLeading: false,
         ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+              progressColors: const ProgressBarColors(
+                playedColor: Colors.red,
+                handleColor: Colors.redAccent,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
