f7945d39af8ca41cd010f6ca354bbdec7e5abd77
tahaglbz <tahaglbz1@gmail.com>
2024-09-09 14:10:11 +0300

    post operations started

--- lib/screens/Forum/forum.dart
+++ lib/screens/Forum/forum.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_app/extensions/media_query.dart';
-
+import '../../widgets/ForumBottomNav.dart';
 import '../../widgets/appColors.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class Forum extends StatefulWidget {
   const Forum({super.key});
@@ -12,11 +17,149 @@ class Forum extends StatefulWidget {
 }
 
 class _ForumState extends State<Forum> {
+  int _selectedIndex = 4;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (_selectedIndex) {
+      case 0:
+        Get.offAllNamed('/mainmenu');
+        break;
+      case 1:
+        _showPostBottomSheet(context); // Open the bottom sheet
+        break;
+      case 2:
+        Get.offAllNamed('/profile');
+        break;
+    }
+  }
+
+  void _showPostBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(25.0)),
+      ),
+      builder: (context) {
+        final TextEditingController textController = TextEditingController();
+        final TextEditingController titleController = TextEditingController();
+
+        final ImagePicker picker = ImagePicker();
+        XFile? selectedImage;
+
+        final bottomInset = MediaQuery.of(context).viewInsets.bottom;
+
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            top: 16.0,
+            bottom: bottomInset + 16.0, // Add padding for the keyboard
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text('Create a Post', style: TextStyle(fontSize: 20)),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: titleController,
+                  decoration: const InputDecoration(
+                    labelText: 'Post Title',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                TextField(
+                  controller: textController,
+                  decoration: const InputDecoration(
+                    labelText: 'Post Text',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton.icon(
+                  icon: const Icon(Icons.photo),
+                  label: const Text('Select Image'),
+                  onPressed: () async {
+                    try {
+                      selectedImage =
+                          await picker.pickImage(source: ImageSource.gallery);
+                      if (selectedImage != null) {
+                        print('Selected image path: ${selectedImage!.path}');
+                      } else {
+                        print('No image selected.');
+                      }
+                    } catch (e) {
+                      Get.snackbar('Error', 'Failed to pick image: $e');
+                    }
+                  },
+                ),
+                const SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (selectedImage != null &&
+                        textController.text.isNotEmpty) {
+                      try {
+                        await _createPost(selectedImage!, textController.text,
+                            titleController.text);
+                        Get.back(); // Close BottomSheet
+                      } catch (e) {
+                        Get.snackbar('Error', 'Failed to create post: $e');
+                      }
+                    } else {
+                      Get.snackbar('Error',
+                          'Please select an image and enter some text.');
+                    }
+                  },
+                  child: const Text('Post'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _createPost(
+      XFile image, String postText, String titleText) async {
+    try {
+      // Resmi Firebase Storage'a yükle
+      String fileName = path.basename(image.path);
+      File imageFile = File(image.path);
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference ref = storage.ref().child('posts/$fileName');
+
+      UploadTask uploadTask = ref.putFile(imageFile);
+      TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+
+      // Resmin URL'ini al
+      String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+
+      // Postu Firestore'a kaydet
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+      await firestore.collection('posts').add({
+        'title': titleText,
+        'text': postText,
+        'imageUrl': downloadUrl,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+
+      Get.snackbar('Success', 'Post created successfully.');
+    } catch (e) {
+      Get.snackbar('Error', 'Failed to create post: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final double deviceWidth = context.deviceWidth;
-    // final double deviceHeight = context.deviceHeight;
     double appBarHeight = deviceWidth * 0.28;
+
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(appBarHeight),
@@ -28,12 +171,14 @@ class _ForumState extends State<Forum> {
             icon: const Icon(Icons.arrow_back_ios_new_sharp),
           ),
           elevation: 0,
-          title: Image.asset(
-            'lib/assets/logo.png',
-          ),
+          title: Image.asset('lib/assets/logo.png'),
           centerTitle: true,
           backgroundColor: AppColors.defaultColor,
         ),
+      ),
+      bottomNavigationBar: ForumBottom(
+        selectedIndex: _selectedIndex,
+        onItemTapped: _onItemTapped,
       ),
     );
   }
---
+++ lib/widgets/ForumBottomNav.dart
@@ -0,0 +1,51 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:my_app/widgets/appColors.dart'; // Import your custom color class
+
+class ForumBottom extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onItemTapped;
+
+  const ForumBottom({
+    super.key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      notchMargin: 8.0,
+      child: Container(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.home),
+              color:
+                  selectedIndex == 0 ? AppColors.defaultColor : Colors.orange,
+              onPressed: () => onItemTapped(0),
+            ),
+            IconButton(
+              icon: const Icon(Icons.add_box),
+              color: selectedIndex == 1
+                  ? AppColors.defaultColor
+                  : AppColors.defaultColor,
+
+              onPressed: () => onItemTapped(1), // Correctly pass the index 1
+            ),
+            IconButton(
+              icon: const Icon(Icons.person_2_sharp),
+              color:
+                  selectedIndex == 2 ? AppColors.defaultColor : Colors.orange,
+              onPressed: () => onItemTapped(2), // Correctly pass the index 2
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
